2d0b9303a2c7f6a8a31897bc7e82f95139734932
shaddygarg <shaddygarg@yahoo.in>
2017-02-07 03:14:54 +0530

    SQL WRITTEN

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var mysql = require('mysql')
+var mysql = require('mysql');
 
 var connection = mysql.createConnection({
 	host: 'localhost',
@@ -12,15 +12,27 @@ var connection = mysql.createConnection({
 })
 
 connection.connect(function(err) {
+	var isbn="123",name="123",author="ld",description="dij",img="djk";
 	if (err) throw err
 		console.log('You are now connected')
-	connection.query('INSERT into books(isbn,author,img) values (12,12,12)',function(err,results){
+	connection.query('INSERT INTO books(ISBN,name,author,description,img) VALUES (?,?,?,?,?)',[isbn,name,author,description,img],function(err,results){
 		if(err) throw err
-			console.log('INSERTED')
-		connection.query('select * from books',function(err,results){
-			if(err) throw err
-				console.log(results)
-		})
+			connection.query('SELECT COUNT(*) AS r FROM books',function(err,results){
+				if(err) throw err
+					var x = results;
+				var n = x[0].r;
+				connection.query('SELECT * FROM books',function(err,results){
+					if(err) throw err
+						for(var i=0;i<n;i++){
+							console.log(i+1+".");
+							console.log(results[i].ISBN);
+							console.log(results[i].name);
+							console.log(results[i].author);
+							console.log(results[i].description);
+							console.log(results[i].img);
+						}
+					})
+			})
 	})
 })
 
