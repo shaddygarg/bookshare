9a5b9f59d513c746618b5ef9b9e6e730a9007751
VishalCR7 <vs383885@outlook.com>
2017-02-06 23:31:51 +0530

    hjads

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var request = require('request');
+var Q = require('q');
+function getbook(name) {
+  var deferred = Q.defer();
+  request("http://pokeapi.co/api/v1/pokemon/"+name, function (error, response, body) {
+    if (response.statusCode == 200) {
+      deferred.resolve(JSON.parse(body));
+    } else {
+      deferred.reject(new Error("Error Getting Pokemon"));
+    }
+  })
+  return deferred.promise;
+}
+
 
 app.use(express.static(__dirname + '/public'));
 
@@ -12,5 +26,16 @@ app.get('/', function(req , res){
 http.listen(8080, function(){
 	console.log("Server running");
 });
+
+io.on('connection', function(socket){
+	console.log("a user connected");
+
+	socket.on('disconnect', function(){
+		console.log("a user disconnected");
+	});
+	
+
+	});
+
 
 
